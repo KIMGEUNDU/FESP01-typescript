83596a3569027adc803f74e5b0040dd27983c73d
KIMGEUNDU <vlkoklv@naver.com>
2023-11-01 16:15:34 +0900

    11월 1일 5교시

--- workspace/ch01/todo.ts
+++ workspace/ch01/todo.ts
@@ -1,6 +1,8 @@
 'use strict';
-(() => {
+(():void => {
   // TODO: 에러나 경고가 발생하는 부분에 적절한 타입 선언
+  
+  // TODO: Todo Type 지정
   type Todos = {
     id: number,
     title: string,
@@ -8,6 +10,7 @@
   };
 
 
+  // eslint-disable-next-line prefer-const
   let todoItems: Todos[];
 
   // api
@@ -19,7 +22,7 @@
     ];
     return todos;
   }
-  // TODO: Todo Type 지정
+
   // crud methods
   function fetchTodos(): Todos[] {
     const todos = fetchTodoItems();
@@ -44,11 +47,20 @@
   }
   function addTwoTodoItems(): void {
     // TODO: addTodo() 함수를 두 번 호출하여 todoItems에 할일 2개 추가
-    const todo1 = { id: 4, title: '안녕4', done: false }
-    const todo2 = { id: 5, title: '안녕5', done: true }
+    const todo1: Todos = { id: 4, title: '안녕4', done: false }
+    const todo2: Todos = { id: 5, title: '안녕5', done: true }
 
     addTodo(todo1)
     addTodo(todo2)
+
+    const first: Todos = logFirstTodo()
+    console.log(first);
+
+    deleteTodo(4)
+    completeTodo(3, todoItems[3])
+
+    const result = showCompleted()
+    console.log(result);
   }
   // 1. Todo 목록을 가져온다.
   todoItems = fetchTodos();
