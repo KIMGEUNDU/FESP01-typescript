7985787bff130ed913239fb283fa7f6e15be7118
KIMGEUNDU <vlkoklv@naver.com>
2023-11-02 16:03:54 +0900

    11월 2일 5교시

--- workspace/ch02/ex02-06/app.ts
+++ workspace/ch02/ex02-06/app.ts
@@ -1,6 +1,17 @@
 import type { Data } from './math';
+import MyMath from './math.js'
+import { sum, substract } from './math.js'
 
 var data: Data = {
   a: 10,
   b: 20
 }
+
+console.log(1, MyMath.sum(data));
+console.log(1, MyMath.substract(data));
+console.log(1, MyMath.divide(data));
+
+console.log(2, sum(data));
+console.log(2, substract(data));
+
+
--- workspace/ch02/ex02-09.ts
+++ workspace/ch02/ex02-09.ts
@@ -1,3 +1,24 @@
-(()=>{
+(() => {
+  interface CityDropdown {
+    value: string;
+    selected: boolean;
+  }
 
+  interface ZipcodeDropdown {
+    value: number;
+    selected: boolean;
+  }
+
+  function createDropdown(list: CityDropdown[]): string {
+    return
+  }
+
+  var cityList: CityDropdown[] = [
+    { value: '서울시', selected: false },
+    { value: '인천시', selected: true },
+    { value: '광주시', selected: false },
+  ]
+
+  const cityTag = createDropdown(cityList)
+  console.log(cityTag);
 })();
